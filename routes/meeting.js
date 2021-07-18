b0507354bf97e53336aabb9ebfd69c4a42441299
Rohail <rohail384@gmail.com>
2021-07-18 15:11:46 +0800

    env variable name changed

--- routes/meeting.js
+++ routes/meeting.js
@@ -4,11 +4,11 @@ const { v4: uuid } = require("uuid");
 const AWS = require("aws-sdk");
 const region = "us-east-1";
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
 const chime = new AWS.Chime({ region });
-chime.endpoint = new AWS.Endpoint(process.env.API_URL);
+chime.endpoint = new AWS.Endpoint(process.env.CHIME_API_URL);
 
 router.get("/", async (req, res) => {
     try {
